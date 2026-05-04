75ed511ccd6c5777dc4e2f2607c25d2b235929db
SamarpitaPattnaik <samarpita.pattnaik@hcltech.com>
2026-05-04 10:56:34 +0530

    Update backend.tf

--- main/backend.tf
+++ main/backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "my-unique-tf-state-12345"
+    bucket         = "terraform-state-333982363626-ap-south-1-an"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
